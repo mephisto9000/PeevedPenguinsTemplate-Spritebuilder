d4cfc8d986f774e71268353a67bf3c19052f45f0
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-08 12:52:24 +0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Mus Bai on 08.07.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,25 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Mus Bai on 08.07.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+
+-(id) init {
+    self = [super init];
+    
+    if (self)
+    {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
