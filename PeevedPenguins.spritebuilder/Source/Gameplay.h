b6aefff571616c7735f17c9608a8ece7dbbbc47c
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-12 11:22:14 +0600

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Mus Bai on 12.07.14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
