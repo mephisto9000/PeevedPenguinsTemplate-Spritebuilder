1b9b1ae814d23db1e855fbabd00ee6438d3d9fb8
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-13 19:20:26 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -31,7 +31,7 @@
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild: level];
     
-    _physicsNode.debugDraw = TRUE;
+    //_physicsNode.debugDraw = TRUE;
     
     _pullbackNode.physicsBody.collisionMask = @[];
     
