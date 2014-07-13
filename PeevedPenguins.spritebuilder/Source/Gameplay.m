f0ec18abe759544062ddc8bdbf77b9e489a74a83
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-13 09:58:50 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -12,10 +12,14 @@
 {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
+    CCNode *_levelNode;
 }
 
 -(void) didLoadFromCCB {
     self.userInteractionEnabled = TRUE;
+    
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild: level];
 }
 
 -(void) touchBegan:(UITouch *) touch withEvent:(UIEvent *) event {
