f8745f82ce09599665e25e1b857506552ded2e02
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-13 12:28:01 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -38,6 +38,11 @@
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce: force];
     
+    self.position = ccp(0,0);
+    CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
+    
+    [self runAction:follow];
+    
 }
 
 @end
