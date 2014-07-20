53bf04aa494d66c72be720db5bc902fdfc95f074
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-20 12:00:42 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -141,7 +141,7 @@
     _currentPenguin = nil;
     [_contentNode stopAction:_followPenguin];
     
-    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration: 1.f position: ccp(0,0)];
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration: 1.f position: ccp(1,0)];
     [_contentNode runAction:actionMoveTo];
 }
 
