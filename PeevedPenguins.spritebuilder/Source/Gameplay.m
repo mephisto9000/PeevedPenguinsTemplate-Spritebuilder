c2024d4a30895ec11c2ad65253af0a1c6e64c51c
mephisto9000 <Mephisto@MacBook-Pro-Mus-Bai.local>
2014-07-20 12:11:24 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -129,7 +129,7 @@
         
         //CCActionFollow *follow = [CCActionFollow actionWithTarget: _currentPenguin worldBoundary:self.boundingBox];
         //[_contentNode runAction:follow];
-         
+        
         _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
         [_contentNode runAction:_followPenguin];
         
@@ -145,7 +145,7 @@
     _currentPenguin = nil;
     [_contentNode stopAction:_followPenguin];
     
-    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration: 0.f position: ccp(0,0)];
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration: 1.f position: ccp(0,0)];
     [_contentNode runAction:actionMoveTo];
 }
 
